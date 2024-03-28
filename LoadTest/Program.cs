c3554b71761b72f301855178fdfe400d191c469b
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-03-28 13:57:33 +0300

    Add load test project

---
+++ LoadTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+HttpClient httpClient = new();
+
+while (true)
+{
+	try
+	{
+		await Task.Delay(Random.Shared.Next(0, 1000));
+
+		var products = await httpClient.GetFromJsonAsync<string[]>("http://shop:8080/products");
+		if (products == null)
+			continue;
+
+		var productIndex = Random.Shared.Next(0, products.Length);
+		var product = products[productIndex];
+
+		await httpClient.PostAsync($"http://shop:8080/products/buy?product={product}", null);
+	}
+	catch
+	{
+		// ignore
+	}
+}

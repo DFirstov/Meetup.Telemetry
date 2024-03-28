a4cf0861aac81fcf154de640e0caa8c2fb31ca70
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-03-28 13:23:12 +0300

    Add Shop project

---
+++ Shop/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+var app = builder.Build();
+
+app.MapGet("/products", (HttpClient httpClient) =>
+{
+	return httpClient.GetFromJsonAsync<string[]>("http://stock:8080/products");
+});
+
+app.MapPost("/products/buy", async (string product, HttpClient httpClient) =>
+{
+	var sum = Random.Shared.Next(100, 10000);
+	
+	await httpClient.PostAsync($"http://stock:8080/products/reserve/{product}", null);
+	await httpClient.PostAsync($"http://payments:8080/pay/{sum}", null);
+
+	return Results.Ok($"The product {product} was bought for {sum} roubles");
+});
+
+app.Run();

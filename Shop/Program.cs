c0b533a17db8bdc84bbd363e28b9a5e3b0837874
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-03-29 08:13:26 +0300

    Handle bad request results in Shop

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -14,8 +14,19 @@ app.MapPost("/products/buy", async (string product, HttpClient httpClient) =>
 {
 	var sum = Random.Shared.Next(100, 10000);
 	
-	await httpClient.PostAsync($"http://stock:8080/products/reserve/{product}", null);
-	await httpClient.PostAsync($"http://payments:8080/pay/{sum}", null);
+	var reservationResult = await httpClient.PostAsync($"http://stock:8080/products/reserve/{product}", null);
+	if (!reservationResult.IsSuccessStatusCode)
+	{
+		var reservationFailureMessage = reservationResult.Content.ReadAsStringAsync();
+		return Results.BadRequest(reservationFailureMessage);
+	}
+
+	var paymentResult = await httpClient.PostAsync($"http://payments:8080/pay/{sum}", null);
+	if (!paymentResult.IsSuccessStatusCode)
+	{
+		var paymentFailureMessage = paymentResult.Content.ReadAsStringAsync();
+		return Results.BadRequest(paymentFailureMessage);
+	}
 
 	return Results.Ok($"The product {product} was bought for {sum} roubles");
 });

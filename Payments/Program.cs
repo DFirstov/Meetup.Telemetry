7208cb13581e92d35466fbd90787dca5a9849189
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2024-03-29 08:14:24 +0300

    Generate more information about the client’s account

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -4,16 +4,30 @@ builder.Logging.ClearProviders(); // disable default logging
 
 var app = builder.Build();
 
-app.MapPost("/pay/{sum:decimal}", (decimal sum) =>
-	{
-		var clientBalance = Random.Shared.Next(0, (int)(10 * sum));
-		
-		if (clientBalance < sum)
-		{
-			return Results.BadRequest("Not enough money on the account");
-		}
+string[] clients = ["John", "Paul", "George", "Ringo", "Yoko", "Pete", "Mick", "Keith", "Charlie", "Ronnie", "David"];
 
-		return Results.Ok("Payment succeeded");
-	});
+app.MapPost("/pay/{sum:decimal}", (decimal sum) =>
+{
+	var clientAccount = GetClientAccount(sum);
+	if (clientAccount.Balance < sum)
+	{
+		return Results.BadRequest($"Not enough money on the account {clientAccount.ClientName}");
+	}
+
+	return Results.Ok($"Payment of the client {clientAccount.ClientName} succeeded");
+});
 
 app.Run();
+
+ClientAccount GetClientAccount(decimal sum)
+{
+	var clientIndex = Random.Shared.Next(0, clients.Length);
+	var clientName = clients[clientIndex];
+	var clientBalance = Random.Shared.Next(0, (int)(10 * sum));
+
+	return new ClientAccount(clientName, clientBalance);
+}
+
+record ClientAccount(
+	string ClientName,
+	decimal Balance);
